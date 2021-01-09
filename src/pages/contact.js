2ebef99876c015a6f62f330c4fea7e7f97fc13ae
Ula Chao <gary0626@gmail.com>
2021-01-09 21:17:07 +0800

    feat(contact): add contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Contact() {
+    return(
+        <div>
+            <h1>I'd love to talk! Email me at the address below</h1>
+            <a href="mailto:gary0626@gmail.com">hello@gmail.com</a>
+        </div>
+    )
+}
+
+export default Contact
