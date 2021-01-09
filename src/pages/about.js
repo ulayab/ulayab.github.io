21c6888a069143dc30e42942f4130a3490e818fd
Ula Chao <gary0626@gmail.com>
2021-01-09 21:10:05 +0800

    feat(about): add about page

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>Hi, Its me.</p>
+        </div>
+    )
+}
+
+export default About
