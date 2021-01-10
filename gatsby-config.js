c4c62124c37d65e6f054569f04c974eac7bcdf00
Ula Chao <gary0626@gmail.com>
2021-01-10 21:33:50 +0800

    deat(gatsby-source-filesystem): impl in code

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,5 +13,12 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
   ],
 }
