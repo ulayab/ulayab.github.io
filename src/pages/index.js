227545e9a412b787a7ff76e798855c08f618ec2b
Ula Chao <gary0626@gmail.com>
2021-01-10 22:05:16 +0800

    feat: move the query logic to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,44 @@
 import React from "react"
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
 
-export default function Home() {
+export default function Home({data}) {
+  console.log('data: ',data)
   return (
     <Layout>
-      <div>Hello world!</div>
+      <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <div key={node.id}>
+            <h3>
+              {node.frontmatter.title}{" "}
+              <span>
+                — {node.frontmatter.date}
+              </span>
+            </h3>
+            <p>{node.excerpt}</p>
+          </div>
+        ))}
     </Layout>
   )
 }
+
+export const query = graphql`
+query MyQuery {
+  allMarkdownRemark {
+    edges {
+      node {
+        id
+        frontmatter {
+          date
+          title
+        }
+        excerpt
+        html
+        timeToRead
+      }
+    }
+    totalCount
+  }
+}
+
+`
